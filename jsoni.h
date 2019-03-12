fbad522afe8fb1fc4d38566f447662367b86edde
Gustav <sir.gustav.the.coder@gmail.com>
2019-03-12 21:48:34 +0100

    WIP implementation from scratch project

---
+++ jsoni.h
@@ -0,0 +1,717 @@
+#ifndef JSONI_H
+#define JSONI_H
+
+#include <vector>
+#include <map>
+#include <string>
+#include <memory>
+
+#include <cassert>
+
+// util types
+struct Location;
+struct Visitor;
+struct Error;
+struct ParseResult;
+
+// json types
+struct Value;
+struct Object;
+struct Array;
+struct String;
+struct Number;
+struct Bool;
+struct Null;
+struct Int;
+
+struct Location
+{
+  Location() : line(-1), column(-1) {}
+  explicit Location(int l) : line(l), column(-1) {}
+  Location(int l, int c) : line(l), column(c) {}
+
+  int line;
+  int column;
+};
+
+std::ostream& operator<<(std::ostream& s, const Location& location)
+{
+  s << "(" << location.line << ", " << location.column << ")";
+  return s;
+}
+
+struct Visitor
+{
+  virtual void VisitObject (Object* object ) = 0;
+  virtual void VisitArray  (Array * array  ) = 0;
+  virtual void VisitString (String* string ) = 0;
+  virtual void VisitNumber (Number* number ) = 0;
+  virtual void VisitBool   (Bool  * boolean) = 0;
+  virtual void VisitNull   (Null  * null   ) = 0;
+  virtual void VisitInt(Int   * integer);
+};
+
+struct Value
+{
+  Location location;
+
+  virtual ~Value() {}
+
+  virtual void Visit(Visitor* visitor) = 0;
+
+  virtual Object* AsObject() { return nullptr; }
+  virtual Array * AsArray () { return nullptr; }
+  virtual String* AsString() { return nullptr; }
+  virtual Number* AsNumber() { return nullptr; }
+  virtual Bool  * AsBool  () { return nullptr; }
+  virtual Null  * AsNull  () { return nullptr; }
+  virtual Int   * AsInt() { return nullptr; }
+};
+
+struct Object : public Value
+{
+  std::map<std::string, std::shared_ptr<Value>> object;
+
+  void Visit(Visitor* visitor) override { visitor->VisitObject(this); }
+
+  Object* AsObject() override { return this; }
+};
+
+struct Array : public Value
+{
+  std::vector<std::shared_ptr<Value>> array;
+
+  void Visit(Visitor* visitor) override { visitor->VisitArray(this); }
+
+  Array* AsArray() override { return this; }
+};
+
+struct String : public Value
+{
+  std::string string;
+
+  void Visit(Visitor* visitor) override { visitor->VisitString(this); }
+
+  String* AsString() override { return this; }
+
+  explicit String(const std::string& s="") : string(s) {}
+};
+
+struct Number : public Value
+{
+  double number;
+
+  void Visit(Visitor* visitor) override { visitor->VisitNumber(this); }
+
+  Number* AsNumber() override { return this; }
+
+  explicit Number(double d) : number(d) {}
+};
+
+struct Bool : public Value
+{
+  bool boolean;
+
+  void Visit(Visitor* visitor) override { visitor->VisitBool(this); }
+
+  Bool* AsBool() override { return this; }
+
+  explicit Bool(bool b) : boolean(b) {}
+};
+
+struct Null : public Value
+{
+  void Visit(Visitor* visitor) override { visitor->VisitNull(this); }
+
+  Null* AsNull() override { return this; }
+};
+
+struct Int : public Number
+{
+  int integer;
+
+  void Visit(Visitor* visitor) override { visitor->VisitInt(this); }
+
+  Int* AsInt() override { return this; }
+
+  explicit Int(int i) : Number(i), integer(i) { }
+};
+
+void Visitor::VisitInt(Int* integer) { VisitNumber(integer); }
+
+struct Error
+{
+  enum class Type
+  {
+    Note,
+    InvalidCharacter,
+    UnclosedArray,
+    UnclosedObject,
+    NotEof,
+    InvalidNumber,
+    IllegalEscape,
+    InvalidCharacterInString,
+    UnexpectedEof,
+    UnknownError
+  };
+  Type type;
+  std::string message;
+  Location location;
+
+  Error(Type t, const std::string& m, const Location& l=Location()) : type(t), message(m), location(l) {}
+};
+
+std::ostream& operator<<(std::ostream& s, const Error::Type& type)
+{
+  switch (type)
+  {
+#define CASE(x) case Error::Type::x: s << #x; break
+    CASE(Note                     );
+    CASE(InvalidCharacter         );
+    CASE(UnclosedArray            );
+    CASE(UnclosedObject           );
+    CASE(NotEof                   );
+    CASE(InvalidNumber            );
+    CASE(IllegalEscape            );
+    CASE(InvalidCharacterInString );
+    CASE(UnexpectedEof            );
+    CASE(UnknownError             );
+#undef CASE
+  default:
+    s << "<unknown error>";
+    break;
+  }
+  return s;
+}
+
+
+std::ostream& operator<<(std::ostream& s, const Error& error)
+{
+  s << error.type << error.location << ": " << error.message;
+  return s;
+}
+
+// todo: this structure seems weird, make it into something that feels less weird
+struct ParseResult
+{
+  // contains errors if parsing failed
+  std::vector<Error> errors;
+
+  // one of these are non-null if parsing didn't fail
+  std::shared_ptr<Object> object;
+  std::shared_ptr<Array> array;
+
+  bool HasError() const
+  {
+    return object == nullptr && array == nullptr;
+  }
+
+  operator bool() const
+  {
+    return !HasError();
+  }
+};
+
+std::ostream& operator<<(std::ostream& s, const ParseResult& result)
+{
+  if (result.HasError())
+  {
+    s << "Errors:\n";
+    for (const auto& e : result.errors)
+    {
+      s << "  " << e << "\n";
+    }
+  }
+  else
+  {
+    s << "No errors detected, but currently unable to print data";
+  }
+  return s;
+}
+
+ParseResult Parse(const std::string& str);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Implementation and detail..
+
+struct Parser;
+
+struct Parser
+{
+  std::string str;
+  int index = 0;
+
+  explicit Parser(const std::string& s) : str(s) {}
+
+  char Peek(int advance = 0) const
+  {
+    const int i = index + advance;
+    if (index >= str.size()) return 0;
+    return str[i];
+  }
+
+  bool HasMoreChar() const
+  {
+    return index < str.size();
+  }
+
+  int line = 0;
+  int column = 0;
+
+  char Read()
+  {
+    const char c = str[index];
+    if (c == '\n')
+    {
+      line += 1;
+      column = 0;
+    }
+    else
+    {
+      column += 1;
+    }
+    index += 1;
+    return c;
+  }
+};
+
+bool IsSpace(char c)
+{
+  if (c == ' ') return true;
+  if (c == '\t') return true;
+  if (c == '\n') return true;
+  if (c == '\r') return true;
+  return false;
+}
+
+void SkipSpaces(Parser* parser)
+{
+  while (IsSpace(parser->Peek()))
+  {
+    parser->Read();
+  }
+}
+
+#define EXPECT(expected_string) do { char c = parser->Read(); if( c != expected_string[0] ) { AddError(result, parser, Error::Type::InvalidCharacter, "Expected character " expected_string); return nullptr; } SkipSpaces(parser); } while(false)
+
+void AddError(ParseResult* result, Parser* parser, Error::Type type, const std::string& err)
+{
+  result->errors.push_back(Error{type, err, Location{parser->line, parser->column} });
+  result->array = nullptr;
+  result->object = nullptr;
+}
+
+std::shared_ptr<Value > ParseValue  (ParseResult* result, Parser* parser);
+std::shared_ptr<Object> ParseObject (ParseResult* result, Parser* parser);
+std::shared_ptr<Array > ParseArray  (ParseResult* result, Parser* parser);
+std::shared_ptr<String> ParseString (ParseResult* result, Parser* parser);
+std::shared_ptr<Number> ParseNumber (ParseResult* result, Parser* parser);
+// std::shared_ptr<Int   > ParseInt    (ParseResult* result, Parser* parser);
+
+bool IsDigit09(char c)
+{
+  return c >= '0' && c <= '9';
+}
+
+bool IsDigit19(char c)
+{
+  return c >= '1' && c <= '9';
+}
+
+bool IsValidFirstDigit(char c);
+
+std::shared_ptr<Value> ParseValue(ParseResult* result, Parser* parser)
+{
+  if (parser->Peek() == '[')
+  {
+    return ParseArray(result, parser);
+  }
+
+  if (parser->Peek() == '{')
+  {
+    return ParseObject(result, parser);
+  }
+
+  if (parser->Peek() == '"')
+  {
+    return ParseString(result, parser);
+  }
+
+  if (
+    parser->Peek(0) == 't' &&
+    parser->Peek(1) == 'r' &&
+    parser->Peek(2) == 'u' &&
+    parser->Peek(3) == 'e'
+    )
+  {
+    parser->Read(); // t
+    parser->Read(); // r
+    parser->Read(); // u
+    parser->Read(); // e
+    SkipSpaces(parser);
+    return std::make_shared<Bool>(true);
+  }
+
+  if (
+    parser->Peek(0) == 'f' &&
+    parser->Peek(1) == 'a' &&
+    parser->Peek(2) == 'l' &&
+    parser->Peek(3) == 's' &&
+    parser->Peek(4) == 'e'
+    )
+  {
+    parser->Read(); // f
+    parser->Read(); // a
+    parser->Read(); // l
+    parser->Read(); // s
+    parser->Read(); // e
+    SkipSpaces(parser);
+    return std::make_shared<Bool>(false);
+  }
+
+  if (
+    parser->Peek(0) == 'n' &&
+    parser->Peek(1) == 'u' &&
+    parser->Peek(2) == 'l' &&
+    parser->Peek(3) == 'l'
+    )
+  {
+    parser->Read(); // n
+    parser->Read(); // u
+    parser->Read(); // l
+    parser->Read(); // l
+    SkipSpaces(parser);
+    return std::make_shared<Null>();
+  }
+
+  if (IsValidFirstDigit(parser->Peek()))
+  {
+    return ParseNumber(result, parser);
+  }
+
+  AddError(result, parser, Error::Type::InvalidCharacter, "Unexpected character found");
+  return nullptr;
+}
+
+std::shared_ptr<Array> ParseArray(ParseResult* result, Parser* parser)
+{
+  enum class State { ExpectStart, ExpectValue, ExpectComma, ExpectEnd, Ended };
+
+  State state = State::ExpectStart;
+
+  auto array = std::make_shared<Array>();
+
+  while(state != State::Ended)
+  { 
+    switch (state)
+    {
+    case State::ExpectStart:
+      EXPECT("[");
+      if (parser->Peek() == ']')
+      {
+        state = State::ExpectEnd;
+      }
+      else
+      {
+        state = State::ExpectValue;
+      }
+      break;
+    case State::ExpectValue:
+      {
+        auto v = ParseValue(result, parser);
+        if (v == nullptr)
+        {
+          return nullptr;
+        }
+        else
+        {
+          array->array.push_back(v);
+        }
+        SkipSpaces(parser);
+        if (parser->Peek() == ',')
+        {
+          state = State::ExpectComma;
+        }
+        else
+        {
+          state = State::ExpectEnd;
+        }
+      }
+      break;
+    case State::ExpectComma:
+      EXPECT(",");
+      state = State::ExpectValue;
+      break;
+    case State::ExpectEnd:
+      EXPECT("]");
+      state = State::Ended;
+    case State::Ended:
+      assert(false && "array loop should have ended");
+      break;
+    }
+  }
+
+  return array;
+}
+
+std::shared_ptr<Object> ParseObject(ParseResult* result, Parser* parser)
+{
+  enum class State { ExpectStart, ExpectValue, ExpectComma, ExpectEnd, Ended };
+
+  State state = State::ExpectStart;
+
+  auto object = std::make_shared<Object>();
+
+  while (state != State::Ended)
+  {
+    switch (state)
+    {
+    case State::ExpectStart:
+      EXPECT("{");
+      if (parser->Peek() == '}')
+      {
+        state = State::ExpectEnd;
+      }
+      else
+      {
+        state = State::ExpectValue;
+      }
+      break;
+    case State::ExpectValue:
+    {
+      auto s = ParseString(result, parser);
+      if (s == nullptr)
+      {
+        return nullptr;
+      }
+      EXPECT(":");
+      auto v = ParseValue(result, parser);
+      if (v == nullptr)
+      {
+        return nullptr;
+      }
+      else
+      {
+        object->object[s->string] = v;
+      }
+      SkipSpaces(parser);
+      if (parser->Peek() == ',')
+      {
+        state = State::ExpectComma;
+      }
+      else
+      {
+        state = State::ExpectEnd;
+      }
+    }
+    break;
+    case State::ExpectComma:
+      EXPECT(",");
+      state = State::ExpectValue;
+      break;
+    case State::ExpectEnd:
+      EXPECT("}");
+      state = State::Ended;
+    case State::Ended:
+      assert(false && "object loop should have ended");
+      break;
+    }
+  }
+
+  return object;
+}
+
+std::shared_ptr<String> ParseString(ParseResult* result, Parser* parser)
+{
+  AddError(result, parser, Error::Type::UnknownError, "Not implemented string parsing...");
+  return nullptr;
+}
+
+std::shared_ptr<Number> ParseNumber(ParseResult* result, Parser* parser)
+{
+  enum class State { ExpectDash, Expect19, ExpectDigit1, Expect0, ExpectDot, ExpectDigit2, ExpectE, Ended };
+  State state = State::Expect0;
+
+  if (parser->Peek() == '-')
+  {
+    state = State::ExpectDash;
+  }
+  else if (IsDigit19(parser->Peek()) )
+  {
+    state = State::Expect19;
+  }
+  else if (parser->Peek() == '0')
+  {
+    state = State::Expect0;
+  }
+  else
+  {
+    AddError(result, parser, Error::Type::InvalidCharacter, "Invalid character");
+  }
+
+  while (state != State::Ended)
+  {
+    switch (state)
+    {
+    case State::ExpectDash:
+      EXPECT("-");
+      if (parser->Peek() == '0')
+      {
+        state = State::Expect0;
+      }
+      else if (IsDigit19(parser->Peek()))
+      {
+        state = State::Expect19;
+      }
+      else
+      {
+        AddError(result, parser, Error::Type::InvalidCharacter, "Invalid character");
+      }
+      break;
+    case State::Expect19:
+    {
+      const char c = parser->Read();
+      if (!IsDigit19(c))
+      {
+        AddError(result, parser, Error::Type::InvalidCharacter, "Invalid character, expected 1-9");
+        return nullptr;
+      }
+
+      if (IsDigit09(parser->Peek()))
+      {
+        state = State::ExpectDigit1;
+      }
+      else if (parser->Peek() == '.')
+      {
+        state = State::ExpectDot;
+      }
+      else
+      {
+        state = State::Ended;
+      }
+    }
+      break;
+    case State::ExpectDigit1:
+    {
+      const char c = parser->Read();
+      if (!IsDigit09(c))
+      {
+        AddError(result, parser, Error::Type::InvalidCharacter, "Invalid character, expected 0-9");
+        return nullptr;
+      }
+
+      if (IsDigit09(parser->Peek()))
+      {
+        // no need to do anything here, just keep looping
+      }
+      else if (parser->Peek() == '.')
+      {
+        state = State::ExpectDot;
+      }
+      else
+      {
+        state = State::Ended;
+      }
+    }
+      break;
+    case State::Expect0:
+      EXPECT("0");
+      if (parser->Peek() == '.')
+      {
+        state = State::ExpectDot;
+      }
+      else if (parser->Peek() == 'e' || parser->Peek() == 'E')
+      {
+        state = State::ExpectE;
+      }
+      else
+      {
+        state = State::Ended;
+      }
+      break;
+    case State::ExpectDot:
+      EXPECT(".");
+      if (IsDigit09(parser->Peek()))
+      {
+        state = State::ExpectDigit2;
+      }
+      else
+      {
+        AddError(result, parser, Error::Type::InvalidCharacter, "Invalid character, expected 1-9");
+        return nullptr;
+      }
+      break;
+    case State::ExpectDigit2:
+    {
+      const char c = parser->Read();
+      if (!IsDigit09(c))
+      {
+        AddError(result, parser, Error::Type::InvalidCharacter, "Invalid character, expected 0-9");
+        return nullptr;
+      }
+      if (IsDigit09(parser->Peek()))
+      {
+        // do nothing
+      }
+      else if (parser->Peek() == 'e' || parser->Peek() == 'E')
+      {
+        state = State::ExpectE;
+      }
+      else
+      {
+        state = State::Ended;
+      }
+    }
+      break;
+    case State::ExpectE:
+      AddError(result, parser, Error::Type::InvalidCharacter, "Not implemented exponent numbers yet");
+      return nullptr;
+      break;
+    case State::Ended:
+      assert(false && "number loop should have ended");
+      break;
+    }
+  }
+
+  // todo: actually parse number...
+  return std::make_shared<Number>(42.0);
+}
+
+bool IsValidFirstDigit(char c)
+{
+  if (c == '-') return true;
+  if (c == '0') return true;
+  if (IsDigit19(c)) return true;
+  return false;
+}
+
+ParseResult Parse(Parser* parser)
+{
+  ParseResult res;
+  SkipSpaces(parser);
+
+  if (parser->Peek() == '[')
+  {
+    ParseArray(&res, parser);
+    return res;
+  }
+  else if(parser->Peek() == '{')
+  {
+    ParseObject(&res, parser);
+    return res;
+  }
+  else
+  {
+    AddError(&res, parser, Error::Type::InvalidCharacter, "Invalid character");
+    return res;
+  }
+}
+
+ParseResult Parse(const std::string& str)
+{
+  Parser parser{ str };
+  return Parse(&parser);
+}
+
+#endif // JSONI_H
+
---
+++ tests.cc
@@ -0,0 +1,423 @@
+#include "catch.hpp"
+#include "jsoni.h"
+
+// tests based on http://json.org/JSON_checker/
+// If the JSON_checker is working correctly, it must accept all of the pass*.json files and reject all of the fail*.json files. 
+
+TEST_CASE("fail1", "[checker]")
+{
+  auto j = Parse(R"("A JSON payload should be an object or array, not a string.")");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail2", "[checker]")
+{
+  auto j = Parse(R"(["Unclosed array")");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::UnclosedArray);
+}
+
+TEST_CASE("fail3", "[checker]")
+{
+  auto j = Parse(R"({unquoted_key: "keys must be quoted"})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail4", "[checker]")
+{
+  auto j = Parse(R"(["extra comma",])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail5", "[checker]")
+{
+  auto j = Parse(R"(["double extra comma",,])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail6", "[checker]")
+{
+  auto j = Parse(R"([   , "<-- missing value"])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail7", "[checker]")
+{
+  auto j = Parse(R"(["Comma after the close"],)");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::NotEof);
+}
+
+TEST_CASE("fail8", "[checker]")
+{
+  auto j = Parse(R"(["Extra close"]])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::NotEof);
+}
+
+TEST_CASE("fail9", "[checker]")
+{
+  auto j = Parse(R"({"Extra comma": true,})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail10", "[checker]")
+{
+  auto j = Parse(R"({"Extra value after close": true} "misplaced quoted value")");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::NotEof);
+}
+
+TEST_CASE("fail11", "[checker]")
+{
+  auto j = Parse(R"({"Illegal expression": 1 + 2})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail12", "[checker]")
+{
+  auto j = Parse(R"({"Illegal invocation": alert()})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail13", "[checker]")
+{
+  auto j = Parse(R"({"Numbers cannot have leading zeroes": 013})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidNumber);
+}
+
+TEST_CASE("fail14", "[checker]")
+{
+  auto j = Parse(R"({"Numbers cannot be hex": 0x14})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail15", "[checker]")
+{
+  auto j = Parse(R"(["Illegal backslash escape: \x15"])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail16", "[checker]")
+{
+  auto j = Parse(R"([\naked])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail17", "[checker]")
+{
+  auto j = Parse(R"(["Illegal backslash escape: \017"])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::IllegalEscape);
+}
+/*
+
+fail18 "depth check" is not added due to it not being in the spec
+https://stackoverflow.com/questions/42116718/is-there-an-array-depth-limitation-in-json
+
+fail18.json:
+[[[[[[[[[[[[[[[[[[[["Too deep"]]]]]]]]]]]]]]]]]]]]
+
+*/
+
+TEST_CASE("fail19", "[checker]")
+{
+  auto j = Parse(R"({"Missing colon" null})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail20", "[checker]")
+{
+  auto j = Parse(R"({"Double colon":: null})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail21", "[checker]")
+{
+  auto j = Parse(R"({"Comma instead of colon", null})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail22", "[checker]")
+{
+  auto j = Parse(R"(["Colon instead of comma": false])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail23", "[checker]")
+{
+  auto j = Parse(R"(["Bad value", truth])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail24", "[checker]")
+{
+  auto j = Parse(R"(['single quote'])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail25", "[checker]")
+{
+  auto j = Parse(R"(["	tab	character	in	string	"])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacterInString);
+}
+
+TEST_CASE("fail26", "[checker]")
+{
+  auto j = Parse(R"(["tab\   character\   in\  string\  "])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacterInString);
+}
+
+TEST_CASE("fail27", "[checker]")
+{
+  auto j = Parse(R"(["line
+break"])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacterInString);
+}
+
+TEST_CASE("fail28", "[checker]")
+{
+  auto j = Parse(R"(["line\
+break"])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::IllegalEscape);
+}
+
+TEST_CASE("fail29", "[checker]")
+{
+  auto j = Parse(R"([0e])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail30", "[checker]")
+{
+  auto j = Parse(R"([0e+])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail31", "[checker]")
+{
+  auto j = Parse(R"([0e+-1])");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+TEST_CASE("fail32", "[checker]")
+{
+  auto j = Parse(R"({"Comma instead if closing brace": true,)");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::UnexpectedEof);
+}
+
+TEST_CASE("fail33", "[checker]")
+{
+  auto j = Parse(R"(["mismatch"})");
+  REQUIRE_FALSE(j);
+  REQUIRE_FALSE(j.errors.empty());
+  INFO(j);
+  REQUIRE(j.errors[0].type == Error::Type::InvalidCharacter);
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("pass1", "[checker]")
+{
+  auto j = Parse(R"(
+[
+    "JSON Test Pattern pass1",
+    {"object with 1 member":["array with 1 element"]},
+    {},
+    [],
+    -42,
+    true,
+    false,
+    null,
+    {
+        "integer": 1234567890,
+        "real": -9876.543210,
+        "e": 0.123456789e-12,
+        "E": 1.234567890E+34,
+        "":  23456789012E66,
+        "zero": 0,
+        "one": 1,
+        "space": " ",
+        "quote": "\"",
+        "backslash": "\\",
+        "controls": "\b\f\n\r\t",
+        "slash": "/ & \/",
+        "alpha": "abcdefghijklmnopqrstuvwyz",
+        "ALPHA": "ABCDEFGHIJKLMNOPQRSTUVWYZ",
+        "digit": "0123456789",
+        "0123456789": "digit",
+        "special": "`1~!@#$%^&*()_+-={':[,]}|;.</>?",
+        "hex": "\u0123\u4567\u89AB\uCDEF\uabcd\uef4A",
+        "true": true,
+        "false": false,
+        "null": null,
+        "array":[  ],
+        "object":{  },
+        "address": "50 St. James Street",
+        "url": "http://www.JSON.org/",
+        "comment": "// /* <!-- --",
+        "# -- --> */": " ",
+        " s p a c e d " :[1,2 , 3
+
+,
+
+4 , 5        ,          6           ,7        ],"compact":[1,2,3,4,5,6,7],
+        "jsontext": "{\"object with 1 member\":[\"array with 1 element\"]}",
+        "quotes": "&#34; \u0022 %22 0x22 034 &#x22;",
+        "\/\\\"\uCAFE\uBABE\uAB98\uFCDE\ubcda\uef4A\b\f\n\r\t`1~!@#$%^&*()_+-=[]{}|;:',./<>?"
+: "A key can be any string"
+    },
+    0.5 ,98.6
+,
+99.44
+,
+
+1066,
+1e1,
+0.1e1,
+1e-1,
+1e00,2e+00,2e-00
+,"rosebud"]
+    )");
+  REQUIRE(j);
+}
+
+TEST_CASE("pass2", "[checker]")
+{
+  auto j = Parse(R"(
+[[[[[[[[[[[[[[[[[[["Not too deep"]]]]]]]]]]]]]]]]]]]
+    )");
+  REQUIRE(j);
+}
+
+TEST_CASE("pass3", "[checker]")
+{
+  auto j = Parse(R"(
+{
+    "JSON Test Pattern pass3": {
+        "The outermost value": "must be an object or array.",
+        "In this test": "It is an object."
+    }
+}
+    )");
+  REQUIRE(j);
+}
+
+// this comes from the spec http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf
+
+TEST_CASE("same-chars", "[spec]")
+{
+  auto j = Parse(R"(["\u002F", "\u002f", "\/", "/"])");
+  REQUIRE(j);
+  REQUIRE(j.array != nullptr);
+  const auto& array = j.array->array;
+  REQUIRE(array.size() == 4);
+
+  auto* a = array[0]->AsString();
+  auto* b = array[1]->AsString();
+  auto* c = array[2]->AsString();
+  auto* d = array[3]->AsString();
+
+  REQUIRE(a);
+  REQUIRE(b);
+  REQUIRE(c);
+  REQUIRE(d);
+
+  const std::string forward_slash = "/";
+  CHECK(a->string == forward_slash);
+  CHECK(b->string == forward_slash);
+  CHECK(c->string == forward_slash);
+  CHECK(d->string == forward_slash);
+}
+
+// todo: add utf8 tests
+// todo: add more weird tests
+// add json terminal app to time the tests https://github.com/serde-rs/json-benchmark
+
