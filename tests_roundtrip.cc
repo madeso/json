03984e0bd01d4436c893a12ce4e5af2f3e390c3b
Gustav <sir.gustav.the.coder@gmail.com>
2019-05-03 23:13:15 +0200

    started adding checks to the various tests

--- tests_roundtrip.cc
+++ tests_roundtrip.cc
@@ -6,136 +6,478 @@
 TEST_CASE("roundtrip01", "[roundtrip]")
 {
   const std::string src = R"([null])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto n1 = aj1->array[0]->AsNull();
+  auto n2 = aj2->array[0]->AsNull();
+
+  REQUIRE(n1);
+  REQUIRE(n2);
 }
 
 TEST_CASE("roundtrip02", "[roundtrip]")
 {
   const std::string src = R"([true])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto b1 = aj1->array[0]->AsBool();
+  auto b2 = aj2->array[0]->AsBool();
+
+  REQUIRE(b1);
+  REQUIRE(b2);
+
+  REQUIRE(b1->boolean);
+  REQUIRE(b2->boolean);
 }
 
 TEST_CASE("roundtrip03", "[roundtrip]")
 {
   const std::string src = R"([false])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto b1 = aj1->array[0]->AsBool();
+  auto b2 = aj2->array[0]->AsBool();
+
+  REQUIRE(b1);
+  REQUIRE(b2);
+
+  REQUIRE_FALSE(b1->boolean);
+  REQUIRE_FALSE(b2->boolean);
 }
 
 TEST_CASE("roundtrip04", "[roundtrip]")
 {
   const std::string src = R"([0])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto i1 = aj1->array[0]->AsInt();
+  auto i2 = aj2->array[0]->AsInt();
+
+  REQUIRE(i1);
+  REQUIRE(i2);
+
+  REQUIRE(i1->integer == 0);
+  REQUIRE(i2->integer == 0);
 }
 
 TEST_CASE("roundtrip05", "[roundtrip]")
 {
   const std::string src = R"(["foo"])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto s1 = aj1->array[0]->AsString();
+  auto s2 = aj2->array[0]->AsString();
+
+  REQUIRE(s1);
+  REQUIRE(s2);
+
+  REQUIRE(s1->string == "foo");
+  REQUIRE(s2->string == "foo");
 }
 
 TEST_CASE("roundtrip06", "[roundtrip]")
 {
   const std::string src = R"([])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 0);
+  REQUIRE(aj2->array.size() == 0);
 }
 
 TEST_CASE("roundtrip07", "[roundtrip]")
 {
   const std::string src = R"({})";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto oj1 = j1.value->AsObject();
+  auto oj2 = j2.value->AsObject();
+
+  REQUIRE(oj1);
+  REQUIRE(oj2);
+
+  REQUIRE(oj1->object.size() == 0);
+  REQUIRE(oj2->object.size() == 0);
 }
 
 TEST_CASE("roundtrip08", "[roundtrip]")
 {
   const std::string src = R"([0,1])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 2);
+  REQUIRE(aj2->array.size() == 2);
+
+  auto i1 = aj1->array[0]->AsInt();
+  auto i2 = aj2->array[0]->AsInt();
+  REQUIRE(i1);
+  REQUIRE(i2);
+  REQUIRE(i1->integer == 0);
+  REQUIRE(i2->integer == 0);
+
+  i1 = aj1->array[1]->AsInt();
+  i2 = aj2->array[1]->AsInt();
+  REQUIRE(i1);
+  REQUIRE(i2);
+  REQUIRE(i1->integer == 1);
+  REQUIRE(i2->integer == 1);
 }
 
 TEST_CASE("roundtrip09", "[roundtrip]")
 {
   const std::string src = R"({"foo":"bar"})";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto oj1 = j1.value->AsObject();
+  auto oj2 = j2.value->AsObject();
+
+  REQUIRE(oj1);
+  REQUIRE(oj2);
+
+  REQUIRE(oj1->object.size() == 1);
+  REQUIRE(oj2->object.size() == 1);
+
+  auto f1 = oj1->object["foo"];
+  auto f2 = oj2->object["foo"];
+
+  REQUIRE(f1);
+  REQUIRE(f2);
+
+  auto v1 = f1->AsString();
+  auto v2 = f2->AsString();
+
+  REQUIRE(v1);
+  REQUIRE(v2);
+
+  REQUIRE(v1->string == "bar");
+  REQUIRE(v2->string == "bar");
 }
 
 TEST_CASE("roundtrip10", "[roundtrip]")
 {
   const std::string src = R"({"a":null,"foo":"bar"})";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto oj1 = j1.value->AsObject();
+  auto oj2 = j2.value->AsObject();
+
+  REQUIRE(oj1);
+  REQUIRE(oj2);
+
+  REQUIRE(oj1->object.size() == 2);
+  REQUIRE(oj2->object.size() == 2);
+
+  auto f1 = oj1->object["foo"];
+  auto f2 = oj2->object["foo"];
+
+  REQUIRE(f1);
+  REQUIRE(f2);
+
+  auto v1 = f1->AsString();
+  auto v2 = f2->AsString();
+
+  REQUIRE(v1);
+  REQUIRE(v2);
+
+  REQUIRE(v1->string == "bar");
+  REQUIRE(v2->string == "bar");
+
+  auto a1 = oj1->object["a"];
+  auto a2 = oj2->object["a"];
+
+  REQUIRE(a1);
+  REQUIRE(a2);
+
+  auto n1 = a1->AsNull();
+  auto n2 = a2->AsNull();
+
+  REQUIRE(n1);
+  REQUIRE(n2);
 }
 
 TEST_CASE("roundtrip11", "[roundtrip]")
 {
   const std::string src = R"([-1])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto i1 = aj1->array[0]->AsInt();
+  auto i2 = aj2->array[0]->AsInt();
+
+  REQUIRE(i1);
+  REQUIRE(i2);
+
+  REQUIRE(i1->integer == -1);
+  REQUIRE(i2->integer == -1);
 }
 
 TEST_CASE("roundtrip12", "[roundtrip]")
 {
   const std::string src = R"([-2147483648])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto i1 = aj1->array[0]->AsInt();
+  auto i2 = aj2->array[0]->AsInt();
+
+  REQUIRE(i1);
+  REQUIRE(i2);
+
+  REQUIRE(i1->integer == -2147483648);
+  REQUIRE(i2->integer == -2147483648);
 }
 
 TEST_CASE("roundtrip13", "[roundtrip]")
 {
   const std::string src = R"([-1234567890123456789])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto i1 = aj1->array[0]->AsInt();
+  auto i2 = aj2->array[0]->AsInt();
+
+  REQUIRE(i1);
+  REQUIRE(i2);
+
+  REQUIRE(i1->integer == -1234567890123456789);
+  REQUIRE(i2->integer == -1234567890123456789);
 }
 
 TEST_CASE("roundtrip14", "[roundtrip]")
 {
   const std::string src = R"([-9223372036854775808])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto i1 = aj1->array[0]->AsInt();
+  auto i2 = aj2->array[0]->AsInt();
+
+  REQUIRE(i1);
+  REQUIRE(i2);
+
+  // osx gets a compiler warning here...
+  // this should be fine since this is the lowest a signed 64bit can go
+  // use INT64_C() macro?
+  REQUIRE(i1->integer == -9223372036854775808);
+  REQUIRE(i2->integer == -9223372036854775808);
 }
 
 TEST_CASE("roundtrip15", "[roundtrip]")
 {
   const std::string src = R"([1])";
-  auto j = Parse(src);
-  REQUIRE(j);
-  const auto dmp = ToString(j.value.get(), PrettyPrint::Compact());
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
   REQUIRE(dmp == src);
+
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  auto aj1 = j1.value->AsArray();
+  auto aj2 = j2.value->AsArray();
+
+  REQUIRE(aj1);
+  REQUIRE(aj2);
+
+  REQUIRE(aj1->array.size() == 1);
+  REQUIRE(aj2->array.size() == 1);
+
+  auto i1 = aj1->array[0]->AsInt();
+  auto i2 = aj2->array[0]->AsInt();
+
+  REQUIRE(i1);
+  REQUIRE(i2);
+
+  REQUIRE(i1->integer == 1);
+  REQUIRE(i2->integer == 1);
 }
 
 TEST_CASE("roundtrip16", "[roundtrip]")
