530e1a31b67ee34c781a346bb8e99fe61f7888c5
Gustav <sir.gustav.the.coder@gmail.com>
2019-05-10 01:36:41 +0200

    split osx tests to better run them

--- tests_osx.cc
+++ tests_osx.cc
@@ -1,28 +1,25 @@
 #include "catch.hpp"
 #include "jsonh.h"
 
-TEST_CASE("why does shorter scientific fail to read on osx?", "[crazy][!mayfail]")
+// this is ok...
+TEST_CASE("osx-ok", "[osx]")
 {
-  // this is ok...
-  SECTION("ok")
-  {
-    std::istringstream ssa("2.2250738585072014e-308");
-    double a;
-    ssa >> a;
-    CHECK_FALSE(ssa.fail());
-    CHECK(ssa.eof());
-  }
+  std::istringstream ssa("2.2250738585072014e-308");
+  double a;
+  ssa >> a;
+  CHECK_FALSE(ssa.fail());
+  CHECK(ssa.eof());
+}
 
-  // but this is not?
-  // fails in osx
-  // read looks ok in debugger/inspector
-  SECTION("failes on osx")
-  {
-    std::istringstream ssb("2.22507e-308");
-    double b;
-    ssb >> b;
-    CHECK_FALSE(ssb.fail());
-    CHECK(ssb.eof());
-  }
+// but this is not?
+// fails in osx
+// read looks ok in debugger/inspector
+TEST_CASE("osx-fail", "[osx][!mayfail]")
+{
+  std::istringstream ssb("2.22507e-308");
+  double b;
+  ssb >> b;
+  CHECK_FALSE(ssb.fail());
+  CHECK(ssb.eof());
 }
 
