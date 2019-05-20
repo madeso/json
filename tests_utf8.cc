2afd3daa892397a98d877b1ed59836dd67623cbe
Gustav <sir.gustav.the.coder@gmail.com>
2019-05-20 08:26:35 +0200

    added utf8 tests

--- tests_utf8.cc
+++ tests_utf8.cc
@@ -1,5 +1,34 @@
 #include "catch.hpp"
 #include "jsonh.h"
 
-// todo: add utf8 tests
+TEST_CASE("utf8", "[utf8]")
+{
+  const std::string src = R"(
+{
+  "emoji" : {
+    "😊":"✨",
+    "❤":"😊",
+    "✨":"❤"
+  },
+  "cjk" : {
+    "あ":"っ",
+    "前":"あ",
+    "っ":"前"
+  },
+  "swedish" : {
+    "ö":"Å",
+    "Ä":"ö",
+    "å":"ä"
+  }
+}
+)";
+  auto j1 = Parse(src);
+  REQUIRE(j1);
+
+  const auto dmp = ToString(j1.value.get(), PrettyPrint::Compact());
+  auto j2 = Parse(dmp);
+  REQUIRE(j2);
+
+  // todo: check j1 and j2
+}
 
