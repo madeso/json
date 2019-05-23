c0153f75e5db3a3aed313bb65d21866ae86d9888
Gustav <sir.gustav.the.coder@gmail.com>
2019-05-23 07:38:30 +0200

    split main to main and impl to improve compilation times

---
+++ tests_impl.cc
@@ -0,0 +1,4 @@
+#define JSONH_IMPLEMENTATION
+#include "jsonh.h"
+// this was in tests_main.cc earlier but catch takes too long to compile each change
+
--- tests_main.cc
+++ tests_main.cc
@@ -1,5 +1,3 @@
 #define CATCH_CONFIG_MAIN
-#define JSONH_IMPLEMENTATION
 #include "catch.hpp"
-#include "jsonh.h"
 
